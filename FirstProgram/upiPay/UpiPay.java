2508009bec0965dc12fffe8dca7dab755fba7ab4
Maroofmk <99120703+Maroofmk@users.noreply.github.com>
2025-05-01 17:45:53 +0530

    b1-BranchCreated-basicStrcture-upiPay

---
+++ FirstProgram/upiPay/UpiPay.java
@@ -0,0 +1,7 @@
+package upiPay;
+
+public class UpiPay {
+	public String PayByUpi(String upiId,Double Anount) {
+		return null;
+	}
+}
--- FirstProgram/work/MainWork.java
+++ FirstProgram/work/MainWork.java
@@ -1,7 +1,7 @@
 package work;
 
 public class MainWork {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("Basi Structure");
 	}
 }
